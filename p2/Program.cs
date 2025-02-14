d2f0049532eb2f5f2e03da40944bcfc96e2e7e91
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-14 12:10:55 +0600

    Print Your Bio

---
+++ p2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Bio{
+
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("My name is Alex");
+        Console.WriteLine("I am 25 years old");
+        Console.Write("I am full stack web & app developer.");
+        Console.Write("And currently I work in an IT company. ");
+    }
+}
