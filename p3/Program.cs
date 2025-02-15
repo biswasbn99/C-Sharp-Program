47b13bde9a94e96d6b1ad41e854613a957387086
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-15 22:32:55 +0600

    Using \n,\t,,' ',\ -->all backslash

---
+++ p3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Bio
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("My name is Alex. \n");
+        Console.Write("\t \"I am 33 years old.\" \n");
+        Console.Write("I am a full stack web developer.\n");
+        Console.WriteLine("\'I live in Mars\'");
+        Console.WriteLine("I love my village \\");
+    }
+}
