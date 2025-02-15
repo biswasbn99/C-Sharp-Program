1c9a988e92752257c2a734d5a9f834073ad96929
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-16 00:09:19 +0600

    Variables & Data types

---
+++ p4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Variable
+{
+    public static void Main(string[] args)
+    {
+        string fullName = "Alex Marino";
+        int age = 55;
+        double CGPA = 3.86;
+        bool isRegister = true;
+        char bloodGroup = 'B';
+
+        Console.WriteLine(fullName);
+        Console.WriteLine(age);
+        Console.WriteLine(CGPA);
+        Console.WriteLine(isRegister);
+        Console.WriteLine(bloodGroup);
+    }
+}
+
+
+    
