28b22895d19e10b2a649bd34c8d64a5ec0ee4be7
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-16 00:42:53 +0600

    Variable Declaration

---
+++ p5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Assignment
+{
+    public static void Main(string[] args)
+    {
+        string name = "Apple iPhone 14";
+        double price=320.5;
+        string category="smart phone";
+        bool isAvailable=true;
+        int sold=5;
+
+
+        // Using concatenation
+        Console.WriteLine("Name : " + name);
+
+        // OR using string interpolation (Recommended)
+        Console.WriteLine($"Name : {name}");
+        Console.WriteLine($"Price:$ {price}");
+        Console.WriteLine($"Category:{category}");
+        Console.WriteLine("Sold:"+sold);
+        Console.WriteLine($"Available:{isAvailable}");
+    }
+}
