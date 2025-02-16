dff8a548ccea230477f85114c94a3eb25eb2784b
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-17 01:13:58 +0600

    How to get User Input?

---
+++ p8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Test{
+    public static void Main(string[]orgs){
+    string name;
+    int age;
+    double salary;
+
+        Console.Write("Enter your Name:");
+        name=Console.ReadLine();
+        Console.WriteLine("Name:"+name);
+        Console.Write("Enter your Age:");
+        age=int.Parse(Console.ReadLine());
+        Console.WriteLine("Age:"+age);
+        Console.Write("Enter Your salary:");
+        salary=Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Salary:"+salary);
+    }
+}
