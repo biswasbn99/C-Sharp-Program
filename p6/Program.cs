fe5406e923100320b6a1abceccb30549df8806de
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-16 22:45:52 +0600

    Constant & Multiple Variable

---
+++ p6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Test{
+
+    public static void Main(string[]args){
+
+        string university = "IUBAT";
+        Console.WriteLine(university);
+        university = "Oxford university";
+        Console.WriteLine(university);
+
+        const string nationality ="Bangladeshi"; //we can't use same variable again
+        Console.WriteLine(nationality);
+
+        int num1=10,num2=10,num3=10;
+        int sum1=num1+num2+num3;
+        Console.WriteLine($"Sum1={sum1}");
+
+int num4,num5,num6;
+         num4=num5=num6=30;
+        int sum2=num4+num5+num6;
+        Console.WriteLine($"Sum2= {sum2}");
+
+    }
+}
