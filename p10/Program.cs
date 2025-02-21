678ca97d6e52da585bf91acbf9d919258e47e130
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-21 12:38:19 +0600

    Arithmetic Operator

---
+++ p10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Test{
+    public static void Main(){
+        int num1=20;
+        int num2=6;
+
+        int sum=num1+num2;
+        Console.WriteLine("Addition: " +sum);
+        int sub=num1-num2;
+        Console.WriteLine("Subtraction: "+sub);
+        int div=num1/num2; //if we print before point part only(interger value)
+        Console.WriteLine("division :"+div);
+
+        int rem=num1%num2;
+        Console.WriteLine("Remainder: "+rem);
+        double div1=(double)num1/num2;
+        Console.WriteLine("Division: "+div1.ToString("F3"));
+    }
+}
