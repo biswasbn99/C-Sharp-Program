15b706ccc273bf5f70c3e47d107cb4bc234c8b0a
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-25 23:47:13 +0600

    Factorial of a number

---
+++ n3/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Write a C# program that prompts the user to enter a positive integer N.
+ The program should calculate and display the factorial of N.  */
+
+ using System;
+ namespace Test{
+    class Factorial{
+        public static void Main(){
+            
+            Console.Write("Enter a number: ");
+            int n= Convert.ToInt32(Console.ReadLine());
+            int Fact=1;
+            for(int i=1; i<=n; i++)
+            {
+               Fact *=i;
+            }
+            
+            Console.Write("Factorial of "+ n +" is :"+ Fact);
+        }
+    }
+ }
