1c0dc054c4192dae2d85bf7e2470994c082850be
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-25 20:31:08 +0600

    The Number is even or odd using if else

---
+++ q2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*2. Write a C# program that prompts the user to enter an integer.
+ The program should determine whether the number is even or odd and display 
+ the appropriate message. ---->using If else statement*/
+
+using System;
+
+class Number{
+    public static void Main(){
+       
+       Console.WriteLine("Enter an Integer : ");
+       int n=Convert.ToInt32(Console.ReadLine());
+
+       if(n%2 == 0){
+        Console.WriteLine("The number is even !!");
+       }
+       else{
+        Console.WriteLine("The number is odd !!");
+       }
+
+    }
+}
