a6db7c95349cd64d5a5b6f9e24fec4745d520ce8
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-25 21:54:20 +0600

    The number is a prime number or not

---
+++ n1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* The number is a prime number or not  */
+using System;
+namespace Test{
+    class Prime{
+        public static void Main(){
+            Console.WriteLine("Enter a number:");
+            int n= Convert.ToInt32(Console.ReadLine());
+            int f=0;
+            for(int d=2 ;d<=(n/2); d++)
+            {
+                if(n%d==0){
+                    f++;
+                    break;
+                }
+            }
+            if(f==0){
+                Console.WriteLine(n+" is a prime number");
+            }
+            else{
+                Console.WriteLine(n+" is not a prime number");
+            }
+        }
+    }
+}
