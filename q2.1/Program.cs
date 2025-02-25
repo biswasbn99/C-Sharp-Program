48a0d437f8ff4887e000291a4203aa635126e7da
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-25 20:37:00 +0600

    The number is Even or Odd using Switch case

---
+++ q2.1/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Test{
+class Number{
+    public static void Main(){
+     
+     Console.WriteLine("Enter an integer :");
+     int n=Convert.ToInt32(Console.ReadLine());
+
+     switch(n){
+        case int x when x%2 == 0:
+        Console.WriteLine(n  +  "  is even number !!");
+        break;
+
+        default:
+        Console.WriteLine(n  +  "   is is odd number !!");
+        break;
+     }
+
+    }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
