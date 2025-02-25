95eb05a110b80c591078857d3982e9eac2ebea9f
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-25 22:52:33 +0600

    Print all Prime number within a range

---
+++ n2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Test
+{
+    class Prime
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a range: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+          Console.Write("Prime Number:");
+            for (int i = 2; i <= n; i++)
+            {
+                bool isPrime = true; // Initialize isPrime for each number
+
+                for (int d = 2; d <= (i / 2); d++)
+                {
+                    if (i % d == 0)
+                    {
+                        isPrime = false;
+                        break; // Exit loop early if not prime
+                    }
+                }
+
+                if (isPrime)
+                {
+                    
+                    Console.WriteLine( + i);
+                }
+            }
+        }
+    }
+}
