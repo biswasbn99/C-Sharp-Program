eeca422200c1232c53e17565211b7ae474cea104
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-26 10:34:27 +0600

    The number is palindrome or not

---
+++ n4/Program.cs
@@ -0,0 +1,26 @@
+﻿/*The number is Palindrome number or not */
+using System;
+namespace Test{
+    class Palindrome{
+        public static void Main(){
+            Console.Write("Enter a number : ");
+            int num=Convert.ToInt32(Console.ReadLine());
+            int i =num;
+            int rev= 0;
+            while(i !=0){
+                int R=(i%10);
+                rev=(rev*10)+R;
+                i/=10;
+            }
+            if(rev==num){
+                Console.WriteLine(num + " is a Palindrome Number");
+
+            }
+            else{
+                Console.WriteLine(num + " is not a palindrome number");
+            }
+
+
+        }
+    }
+}
