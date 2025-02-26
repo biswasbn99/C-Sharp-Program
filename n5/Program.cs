8006609f08d619dfdb0d4765e8b571f7db052be1
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-26 11:53:52 +0600

    Print all palindrome number under a range

---
+++ n5/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Print all palindrome number under a range  */
+
+using System;
+namespace Test
+{
+    class Palindrome
+    {
+       public static void Main()
+        {
+            Console.Write("Enter a range: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The Palindrome Number:");
+            
+            for (int i = 1; i <= num; i++)
+            {
+                int temp = i, rev = 0;
+                while (temp != 0)
+                {
+                    int R = temp % 10;
+                    rev = (rev * 10) + R;
+                    temp /= 10;
+                }
+                
+                if (rev == i)
+                {
+                    Console.Write(i +" ");
+                }
+            }
+        }
+    }
+}
