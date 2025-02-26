6bb67ac9febf13b24e7be85d059721b72ee44efd
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-26 23:48:59 +0600

    Print all number within a range

---
+++ n7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Test
+{
+    class Neon
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a range: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("All Neon Numbers within the range:");
+
+            // Loop from 0 to the given range
+            for (int i = 0; i <= num; i++)
+            {
+                int square = i * i;
+                int sum = 0;
+
+                // Sum the digits of the square of the number
+                while (square > 0)
+                {
+                    int R = square % 10;
+                    sum += R;
+                    square /= 10;
+                }
+
+                // If sum of digits equals the number itself, it's a neon number
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
