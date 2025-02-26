1dc3351191c0fc39c1c02f8f5ad37ba41e62c951
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-26 21:03:40 +0600

    The number is Neon number or not

---
+++ n6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Test
+{
+    class Neon
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int square = num * num;
+            int sum = 0;
+            
+            while (square > 0)
+            {
+                int R = square % 10;
+                sum += R;
+                square /= 10;
+            }
+            
+            if (sum == num)
+                Console.WriteLine(num + " is a Neon number");
+            else
+                Console.WriteLine(num + " is not a Neon number");
+        }   
+    }
+}
