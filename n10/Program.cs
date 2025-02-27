44697308f90b40a0c25673bb12cdea1458ccc681
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-27 10:28:01 +0600

    The number is Armstrong or not

---
+++ n10/Program.cs
@@ -0,0 +1,34 @@
+﻿/* The number is armstrong or not */
+
+using System;
+
+namespace Test
+{
+    class Armstrong
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int num = n;  // Store the original number
+            int sum = 0;
+            int length = num.ToString().Length;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sum +=(int)Math.Pow(digit, length);  // Cast the result to int
+                num /= 10;
+            }
+
+            if (sum == n)  // Compare with the original number
+            {
+                Console.WriteLine(n + " is an Armstrong Number!!");
+            }
+            else
+            {
+                Console.WriteLine(n + " is not an Armstrong Number!!");
+            }
+        }
+    }
+}
