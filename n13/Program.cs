0502bd84e28ac6d3e5490524ff61040674c45496
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-02-27 11:10:24 +0600

    Print all Perfect number within a range

---
+++ n13/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Print all Perfect number within a range */
+
+
+using System;
+
+namespace Test
+{
+    class PerfectNumber
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a range: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+           
+            // Print header for perfect numbers
+            Console.Write("All Perfect numbers: ");
+
+            // Outer loop to iterate through numbers from 1 to n
+            for (int num = 1; num <= n; num++)
+            {
+                int sum = 0;
+
+                // Inner loop to find divisors of 'num'
+                for (int i = 1; i <= num / 2; i++)  // Check divisors only up to num/2
+                {
+                    if (num % i == 0)
+                    {
+                        sum += i;  // Add divisor to the sum
+                    }
+                }
+
+                // After calculating sum of divisors, check if it's a perfect number
+                if (sum == num)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+}
