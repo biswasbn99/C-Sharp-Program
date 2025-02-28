682839745135a770c26a4a9bef9d25709c2bd0af
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-03-01 00:08:44 +0600

    Take input array elements from user and print them

---
+++ a2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Array---> Take input from user and print them */
+using System;
+namespace Test{
+    class ABC{
+        public static void Main(){
+            Console.Write("Enter the number of the elements of the array: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int[] numbers=new int[n];
+            Console.WriteLine("Enter the elements of the array: ");
+            for(int i=0;i<numbers.Length;i++){
+                Console.Write("Element -"+ (i+1)+ " :");
+                numbers[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The arrays are:");
+            Console.WriteLine(String.Join(" ",numbers));
+
+            //print using foreach loop:
+            Console.WriteLine("The arrays are:");
+            foreach(int number in numbers){
+
+             Console.WriteLine("Element-" + Array.IndexOf(numbers,number) + " :" +number);
+            }
+        }
+    }
+}
