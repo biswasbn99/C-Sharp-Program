cd78e7984909f477a29662f6911927a3ffc7be32
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-03-01 11:00:00 +0600

    Print multidimentional Array to get input from user

---
+++ a5/Program.cs
@@ -0,0 +1,39 @@
+﻿/*print multidimentional Array to get input from user  */
+using System;
+
+namespace Test
+{
+    class Matrix
+    {
+        public static void Main()
+        {
+            Console.Write("How many rows---->");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine();
+            Console.Write("How many columns----->");
+            int m = Convert.ToInt32(Console.ReadLine());
+            
+            int[,] matrix = new int[n, m];
+            
+            Console.WriteLine("Enter the elements of the matrix:");
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    matrix[row, col] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            
+            Console.WriteLine("The Matrix is:");
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
