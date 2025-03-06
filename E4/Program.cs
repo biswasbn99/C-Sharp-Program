8c25e2662498437b3ab98a0d8f7c3144f37aba86
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-03-06 10:11:43 +0600

    Define an enumeration AccessRights with the Flags attribute that represents different levels  of user permissions (Read = 1, Write = 2, Execute = 4). Then write a C# program that displays   all user permissions.

---
+++ E4/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Define an enumeration AccessRights with the Flags attribute that represents different levels
+ of user permissions (Read = 1, Write = 2, Execute = 4). Then write a C# program that displays
+  all user permissions. */
+  using System;
+
+  namespace Test{
+    public class FlagAttribute{
+       public enum AccessRights{
+        Read=1,
+        Write=2,
+        Execute=3
+       }
+    
+    public void PermissionClass(AccessRights d){
+        switch(d){
+            case AccessRights.Read:
+            Console.WriteLine("This Flag Attribute represents \"Read Operation\" ");
+            break;
+            case AccessRights.Write:
+            Console.WriteLine("This Flag Attribute represents \"Write Operation\"  ");
+            break;
+            case AccessRights.Execute:
+            Console.WriteLine("This Flag Attribute represents the \"Execution\" ");
+            break;
+        }
+    }
+    }
+    class Myclass{
+        static void Main(){
+            FlagAttribute c=new FlagAttribute();
+            c.PermissionClass(FlagAttribute.AccessRights.Read);
+            c.PermissionClass(FlagAttribute.AccessRights.Write);
+            c.PermissionClass(FlagAttribute.AccessRights.Execute);
+        }
+    }
+
+
+  }
