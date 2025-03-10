4c1de5743ccafb672c85eb8e92a5f3009a74cf16
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-03-10 23:43:01 +0600

    Create a class Car with the following members: - A string field Model. - A string field Brand. Define a copy constructor that creates a new instance of the Car class by copying the values from another Car object. Write a program that creates a Car object using the parameterized constructor, then creates a second Car object using the copy constructor, and displays the details of both cars.

---
+++ C3/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Create a class Car with the following members:
+- A string field Model.
+- A string field Brand.
+Define a copy constructor that creates a new instance of the Car class by copying the
+values from another Car object. Write a program that creates a Car object using the 
+parameterized constructor, then creates a second Car object using the copy constructor,
+and displays the details of both cars. */
+using System;
+namespace Test{
+    public class Car{
+        //Fields
+        public string Model{get;set;}
+        public string Brand{get;set;}
+        //Parameterized Constructor
+        public Car(string model,string brand){
+            Model=model;
+            Brand=brand;
+        }
+        //Copy Constructor
+        public Car(Car c){
+            Model=c.Model;
+            Brand=c.Brand;
+        }
+        public void ShowDetails(){
+            Console.WriteLine("Model :"+Model+", Brand :"+Brand);
+        }
+    }
+    public class Myclass{
+        public static void Main(){
+            Car c1=new Car("Corolla","Toyota");
+    Car c2=new Car(c1);
+
+    Console.WriteLine("Display orginal car details: ");
+    c1.ShowDetails();
+    Console.WriteLine("Display Copied car details: ");
+    c2.ShowDetails();
+        }
+    }
+}
