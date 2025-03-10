55d7d804e51f7c667cbd8c8a62c66369a94740b1
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-03-10 19:02:52 +0600

    Neuralink Brain Signal Processing: Scenario: Neuralink’s brain-computer interface processes neural signals from electrodes implanted in a human brain. Sensitive brain activity data should not be accessible outside of the Neuralink AI module. Implement a private protected method in a NeuralProcessor class that allows only derived AI components in the same assembly to process raw neural data securely. Challenge: Ensure that only AI-related classes within the same ecosystem can interpret brain signals while keeping data processing secure from external threats.

---
+++ AM3/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Tesla Self-Driving Car Navigation:
+Scenario: Tesla’s self-driving software needs to retrieve real-time traffic data.
+ However, this data should only be accessible within Tesla’s software ecosystem. 
+ Implement an internal method in a NavigationSystem class that allows only Tesla’s 
+ internal software components to process and analyze real-time traffic conditions.
+ Challenge: Ensure that the navigation data cannot be accessed or modified by external
+ applications or third-party software.
+ */
+ using System;
+ namespace Test{
+     
+     //Base Class
+     public class NavigationSystem{
+        internal void TeslaSelfEcosystem(){
+            Console.WriteLine("Analyze real-time traffic conditions");
+        }
+     }
+     //Derived Class
+     public class AutoProcess:NavigationSystem{
+       public void DataAutoProcess(){
+        Console.WriteLine("Track the traffic Conditions Automatically");
+        TeslaSelfEcosystem();
+       }
+     }
+public class UnAuthorizedAccess{
+    public void TryAccess(){
+        Console.WriteLine("Hackers will not able to enter the system ??");
+    }
+}
+public class Myclass{
+    public static void Main(){
+        AutoProcess process=new AutoProcess();
+        process.DataAutoProcess();
+        UnAuthorizedAccess access=new UnAuthorizedAccess();
+        access.TryAccess();
+    }
+}
+
+ }
