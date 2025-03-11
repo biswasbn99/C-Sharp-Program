e1e78ba4017b1f115415ea834f604b4c1aeffd86
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-03-11 09:39:04 +0600

    Basic Idea of Properties

---
+++ PI1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Basic Idea of Properties */
+using System;
+namespace Test{
+    class Person{
+        private string name;
+        private int age;
+        public string Name{    //Variable must be capital Letter
+            get{return name;}
+            set{name=value;}
+        }
+        public int Age{ //variable must be capital letter
+            get{return age;}
+            set{age=value;}
+
+        }
+        public void DisplayAll(){
+            Console.WriteLine("Name: "+name);
+            Console.WriteLine("Age: "+age);
+        }
+    }
+    public class Myclass{
+        public static void Main(){
+            Person p=new Person();
+            p.Name="Kamal";
+            p.Age=27;
+            p.DisplayAll();
+        }
+    }
+
+}
