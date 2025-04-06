deab14c47cf65287d1ee5ad722e0780a9c43e1a1
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-04-06 09:11:52 +0600

    Single Inheritance:
     Create two classes: Person and Student. The Person class should have the properties
     Name and Age, and a method DisplayInfo() that prints the name and age. The Student
    class should inherit from Person and have an additional property StudentID and method
     DisplayStudentInfo() that prints the student ID along with the name and age. Demonstrate
     single inheritance by creating a Student object.

--- I1/obj/Debug/net9.0/I1.AssemblyInfo.cs
+++ I1/obj/Debug/net9.0/I1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("I1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6a880497d85349d0a278a7044bb5f6403d00255")]
 [assembly: System.Reflection.AssemblyProductAttribute("I1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("I1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
