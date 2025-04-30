e4233c5adeb197d4bbb6e3fe585563f5f96c935f
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-04-30 12:25:39 +0600

    Write a C# program to demonstrate the use of a delegate that takes a string parameter  and displays a message using a method assigned to the delegate.

---
+++ DA1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Write a C# program to demonstrate the use of a delegate that takes a string parameter
+ and displays a message using a method assigned to the delegate.*/
+using System;
+namespace Test
+{
+    public delegate void MyDelegate(string message);
+    public class Myclass
+    {
+        public static void MyBio(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        public static void Main()
+        {
+            MyDelegate myDelegate=new MyDelegate(MyBio);
+            myDelegate("My Name is BN");
+        }
+    }
+}
