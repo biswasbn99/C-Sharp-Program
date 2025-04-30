2efdf3dfbab7c22810e32d1b6ca1e06d12179385
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-04-30 23:44:07 +0600

    Write a C# program to demonstrate the concept of aggregation by creating a Department class and an Employee class where multiple employees can be associated with a single  department.

---
+++ DA3/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Write a C# program to demonstrate the concept of aggregation by creating a Department
+class and an Employee class where multiple employees can be associated with a single
+ department.*/
+ using System;
+ namespace Test
+ {
+    public class Department
+    {
+        public string DepartmentName{get;set;}
+        public Department(string deptName)
+        {
+        DepartmentName=deptName;
+        }
+    }
+    public class Employee
+    {
+       public string EmployeeName{get;set;}
+      public  Department Dept{get;set;}
+       public Employee(string empName,Department dept )
+       {
+        EmployeeName=empName;
+        Dept=dept;
+       }
+       public void displays()
+       {
+        Console.WriteLine("Employee Name: "+EmployeeName+"Department Name: "+Dept.DepartmentName);
+       }
+    }
+    public class Myclass{
+        public static void Main()
+        {
+            Department department=new Department("IT");
+            Employee employee=new Employee("Kamal",department);
+            Employee employee1=new Employee("Jamal",department);
+            employee.displays();
+            employee1.displays();
+
+        }
+    }
+    
+ }
+ 
+
