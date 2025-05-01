35703bf65de91096a84746b3c43654a30903e2a2
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-05-01 07:48:13 +0600

    Write a simple C# program that defines a delegate called PrintMessage which takes a string parameter and returns string. Then, implement a method DisplayMessage that prints a given message to the console. Create an instance of the delegate and use it to call the DisplayMessage method.

---
+++ T6.1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Write a simple C# program that defines a delegate called PrintMessage which takes a string 
+parameter and returns string. Then, implement a method DisplayMessage that prints a given 
+message to the console. Create an instance of the delegate and use it to call the 
+DisplayMessage method.*/
+
+using System;
+namespace Test
+{
+    public delegate string PrintMessage(string message);
+   public class Myclass
+   {
+     public static string DisplayMessage(string msg)
+    {
+       Console.WriteLine("Message : "+msg );
+       return msg; 
+    }
+
+    public static void Main()
+    {
+        PrintMessage printMessage=new PrintMessage(DisplayMessage);
+        printMessage("My Name is Bn");
+    }
+   }
+
+}
