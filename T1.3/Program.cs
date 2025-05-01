f91b8c7649bbf214980d4b15e2836d0203363356
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-05-01 12:33:39 +0600

    Write a C# program that take an integer array from the user and find the second largest number and display the value if possible.

---
+++ T1.3/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Write a C# program that take an integer array from the user and find 
+the second largest number and display the value if possible. */
+using System;
+namespace Test
+{
+    public class Myclass
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("How many elements of the array?");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter all elements: ");
+            int[] numbers=new int[n];
+            for(int i=0;i<numbers.Length;i++)
+            {
+                Console.WriteLine("Element-"+(i+1)+" : ");
+                numbers[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The array:");
+            foreach(int number in numbers)
+            {
+                Console.Write(" "+number);
+            }
+            Console.WriteLine();
+            Console.WriteLine("After sorting array : ");
+            Array.Sort(numbers);
+            foreach(int number in numbers)
+            {
+                Console.Write(number+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Second largest number is :"+numbers[n-2]);
+        }
+    }
+}
