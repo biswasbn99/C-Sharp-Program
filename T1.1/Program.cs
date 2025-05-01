80914201165c53284aa0231a216c3af025d13070
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-05-01 11:34:08 +0600

    Write a C# program to determine whether a given year is a leap year or not.

---
+++ T1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Write a C# program to determine whether a given year is a leap year 
+or not. */
+using System;
+namespace Test
+{
+    public class Myclass
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a year:");
+            int year =Convert.ToInt32(Console.ReadLine());
+            if((year%4==0 && year%100!=0)||(year%400==0))
+            {
+                Console.WriteLine(year+" is a leap year....");
+            }
+            else
+            {
+                Console.WriteLine(year+" is not a leap year....");
+            }
+        }
+    }
+}
