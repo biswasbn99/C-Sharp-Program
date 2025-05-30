acecec9dabc739f3b04521e92d8517efb212e115
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-05-30 21:40:39 +0600

    Given an array named numbers with values {5, 10, 8, 3, 6, 12, 15, 16},
    Search and display all numbers that are divisible by 2
    
    Display the output in ascending order
    
    Finally, write a valid C# program that performs the above task and displays the output.

---
+++ L2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Given an array named numbers with values {5, 10, 8, 3, 6, 12, 15, 16},
+
+Search and display all numbers that are divisible by 2
+
+Display the output in ascending order
+
+Finally, write a valid C# program that performs the above task and displays the output. */
+
+using System;
+using System.Linq;
+namespace Test
+{
+    public class Myclass
+    {
+        public static void Main()
+        {
+            int[] numbers = { 5, 10, 8, 3, 6, 12, 15, 16 };
+
+            var numQuery1 =
+                from num in numbers
+                where num % 2 == 0
+                orderby num
+                select num;
+
+            foreach (var i in numQuery1)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
