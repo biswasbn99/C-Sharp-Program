b562befd628e76582469a9103b9fbab9cf2f9548
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-05-31 00:35:42 +0600

    Given the array int[] factorsOf300 = { 2, 2, 3, 5, 5 };, what does the following C# program output, and how does it determine the number of unique factors of 300?

---
+++ L7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+namespace Test
+{
+   public class Myclass
+{
+    public static void Main()
+    {
+int[] factorsOf300 = { 2, 2, 3, 5, 5 };
+int uniqueFactors = factorsOf300.Distinct().Count();
+Console.WriteLine("There are {0} unique factors of 300.", uniqueFactors);
+
+    }
+} 
+}
