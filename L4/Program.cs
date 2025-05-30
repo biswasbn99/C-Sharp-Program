f3ee92d3afd8b6caecd372f176e6499aba3dd5da
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-05-30 21:43:51 +0600

    Given an array named words = { cherry, apple, blueberry }, Write a C# program to sort the words in ascending alphabetical order and display the result.

---
+++ L4/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Given an array named words = { "cherry", "apple", "blueberry" },
+Write a C# program to sort the words in ascending alphabetical order and display the result.*/
+
+using System;
+using System.Linq;
+
+public class Myclass
+{
+    public static void Main()
+    {
+        string[] words = { "cherry", "apple", "blueberry" };
+
+        var sortWords = from w in words
+                        orderby w
+                        select w;
+
+        Console.WriteLine("The sorted list of words:");
+        foreach (var w in sortWords)
+        {
+            Console.WriteLine(w+" ");
+        }
+    }
+}
