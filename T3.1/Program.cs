0aa7f3ad8d5b8100144d0d481acd7f153f4831f2
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2025-05-30 10:59:43 +0600

    Write a C# program where input type of the shape output is the area of that shape.

---
+++ T3.1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Write a C# program where input type of the shape output is the area of that shape. */
+using System;
+namespace Test
+{
+    public class AllShap
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Input 1 for Triangle.... ");
+            Console.WriteLine("Enter Input 2 for Square.... ");
+            Console.WriteLine(" Enter Input 3 for Rectangle ...");
+            int n=Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    double p = 20;
+                    double m = 30;
+                    Console.WriteLine("Area of Triangle :" + (0.5 * 20 * 30));
+                    break;
+                    case 2:
+                    Console.WriteLine("Enter Side length:");
+                    double k=Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Area of a Square: " + (k * k));
+                    break;  
+                    case 3:
+                    double length = 10;
+                    double width = 20;
+                    Console.WriteLine("Area of a Rectangle :"+(length * width));
+                    break;
+            }
+        }
+    }
+}
